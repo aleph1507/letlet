57a3e5f5582ab49ecdd090eae31015728998b91e
aleph1507 <xrristo@gmail.com>
2018-05-26 21:10:35 +0200

    vrz.approvals0001

--- src/app/approvals/approvals.component.ts
+++ src/app/approvals/approvals.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ApprovalsService } from '../services/approvals.service';
 import { Requester } from '../models/Requester.model';
 import { MatTableDataSource, MatRadioGroup, MatRadioButton, MatRadioGroupBase,
-  MatRadioButtonBase, MatRadioChange } from '@angular/material';
+  MatRadioButtonBase, MatRadioChange, MatDatepickerInputEvent } from '@angular/material';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs/Observable';
 import {startWith} from 'rxjs/operators/startWith';
 import {map} from 'rxjs/operators/map';
 import { ResourcesService } from '../services/resources.service';
+import { ApprovalRequest } from '../models/approvalRequest';
 
 @Component({
   selector: 'app-approvals',
@@ -16,31 +17,77 @@ import { ResourcesService } from '../services/resources.service';
 })
 export class ApprovalsComponent implements OnInit {
 
-  approvalRequests = [];
+  approvalRequests : ApprovalRequest[] = [];
   dataSource;
   displayedColumns = ['date', 'requesterName', 'requesterCompany', 'nPersons',
              'nVehicles', 'status', 'approvedDate', 'approvedFrom', 'reqID'];
-  showApprovals = ['Unverified', 'All', 'Approved', 'Declined'];
-  selectedApprovals = 'Unverified';
+  // showApprovals = ['Unverified', 'All', 'Approved', 'Declined'];
+  showApprovals = [];
+  selectedApprovals = 'All';
   filteredCompanies: Observable<string[]>;
+  dateFrom = null;
+  dateTo = null;
 
   // companies = ['AMC', 'BBC', 'TAV', 'DrinkerLab'];
   companies = this.resourcesService.companies.getCompaniesNames();
   fCompany: FormControl = new FormControl();
 
-
+  // approvalsUrl = 'http://192.168.100.4:84/api/requests/approvals/3/2018-01-01/2019-01-01';
+  approvalsUrl = 'http://192.168.100.4:84/api/requests/approvals/';
 
   constructor(private approvalsService: ApprovalsService,
               private resourcesService: ResourcesService) { }
 
   ngOnInit() {
-    this.approvalRequests = this.approvalsService.formatRequests();
+    // this.approvalRequests = this.approvalsService.formatRequests();
+    this.showApprovals = this.approvalsService.showApprovals;
+    // console.log('showApprovals[0]: ' + this.showApprovals[0]);
     this.dataSource = new MatTableDataSource(this.approvalRequests);
     this.filteredCompanies = this.fCompany.valueChanges
       .pipe(
         startWith(''),
         map(company => this.filterCompanies(company))
       );
+  }
+  fromString : string = null;
+  toString : string = null;
+  getAR(picker, event: MatDatepickerInputEvent<Date>) {
+    var category = (this.showApprovals.indexOf(this.selectedApprovals) + 1).toString();
+
+    var month : string = '';
+    var day : string = '';
+    // approvalsUrl = 'http://192.168.100.4:84/api/requests/approvals/3/2018-01-01/2019-01-01';
+    // this.approvalsUrl += category += '/'
+
+
+    event.value.getMonth() >= 10 ?
+      month = '-' + (event.value.getMonth() + 1).toString() : month = '-0' + (event.value.getMonth() + 1).toString();
+
+    // event.value.get
+    event.value.getDate() >= 10 ?
+      day = '-' + (event.value.getDate()).toString() : day = '-0' + (event.value.getDate()).toString();
+
+      // console.log('month : ' + month + ' ,  day: ' + day);
+
+    picker == 'from' ? this.fromString = event.value.getFullYear() + month + day :
+                      this.toString = event.value.getFullYear() + month + day;
+
+    // console.log('fromString: ' + this.fromString + ', toString: ' + this.toString);
+
+    var aUrl = this.approvalsUrl + category + '/' + this.fromString + '/' + this.toString;
+
+    if(this.fromString != null && this.toString != null){
+      this.approvalsService.getRequests(aUrl)
+        .subscribe((data : ApprovalRequest[]) => {
+          this.approvalRequests = data;
+        })
+    }
+
+    // console.log('aUrl: ' + aUrl);
+
+    // console.log('category: ' + category + ', picker: ' + picker + ', event.value: ' + event.value +
+    //   ', event.value.getDay: ' + event.value.getDay() + ', event.value.getMonth: ' + event.value.getMonth()
+    //   + ', event.value: ' + event.value.getFullYear());
   }
 
   filterCompanies(name: string) {
--- src/app/services/approvals.service.ts
+++ src/app/services/approvals.service.ts
@@ -3,6 +3,8 @@ import { RequesterService } from './requester.service';
 import { Requester } from '../models/Requester.model';
 import { ApprovalRequest } from '../models/approvalRequest';
 import { ResourcesService } from './resources.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ApprovalsService {
@@ -10,15 +12,30 @@ export class ApprovalsService {
   approvalRequests = Array<ApprovalRequest>();
   requests: Requester[];
 
-  constructor(private requesterService: RequesterService,
-              private resourcesService: ResourcesService) { }
+  // approvalsUrl = 'http://192.168.100.4:84/api/requests/approvals/3/2018-01-01/2019-01-01';
+  // 1 - all | 2 - approved | 3- not approved
+  // approvalsUrl = 'http://192.168.100.4:84/api/requests/approvals/3/2018-01-01/2019-01-01';
 
-  getRequests() {
-    this.requests = this.requesterService.getAllRequests();
-    return this.requests;
+  public showApprovals = ['All', 'Approved', 'Declined'];
+  public selectedApprovals: string;
+  headers: HttpHeaders = new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'Bearer ' + 'RhLV2Xd_l5NPR9XEKzIuThoXcUdHLnKhTZqykf96kzMQqNBRZJPc26ZIHNBEjXgVrVgsWSGrk6a0iR1S1RwB7uoUAUfeiWZGnYKGlLoYcll3q0OrDX3HdnEOYd24D0eYkSkWR9s_YJSAnOtNXduNliRZMwY5OXy27UTPdxYAKSe1GMgiyJudlaLl2858EZ4x5EH05B5CySoHn_DizrsNO6RkVZczJvWicarx3AjUkHHGdZZYS5EkvfZ54T01CdCn1pGy6rnJMOrgUPzOtW_6ILsYcr1NlSThyJxWbeNUBxCAlUaV7FQFv_Krl9ZasSZ8g5x5GTTORIY0FvGrk7Kbu6rbkIJjPnZbX0xDVjdDGwW0HI_Y8L0Cjo-iQ2TjWHy3MlvGmogRQhxy-WpA0fCm-A',
+    'Accept': 'application/json'
+  })
+
+  constructor(private requesterService: RequesterService,
+              private resourcesService: ResourcesService,
+              private http: HttpClient) { }
+
+  getRequests(aUrl : string) : Observable<ApprovalRequest[]> {
+    return this.http.get<ApprovalRequest[]>(aUrl, { headers: this.headers });
+    // this.requests = this.requesterService.getAllRequests();
+    // return this.requests;
   }
 
   getApprovalRequests() {
+    // this.http.get<ApprovalRequest[]>()
     return this.approvalRequests;
   }
 
@@ -28,19 +45,19 @@ export class ApprovalsService {
 
 
 
-    for(let i =0; i<this.requests.length; i++){
-      let ar = new ApprovalRequest();
-      ar.date = this.requests[i].date;
-      ar.requesterName = this.requests[i].requesterName;
-      ar.requesterCompany = this.requests[i].company;
-      ar.nPersons = this.requests[i].persons.length;
-      ar.nVehicles = this.requests[i].vehicles.length;
-      ar.status = (i % 2 == 0);
-      ar.approvedDate = Date.now();
-      ar.approvedFrom = i.toString();
-      ar.reqID = this.requests[i].requestID;
-      this.approvalRequests.push(ar);
-    }
+    // for(let i =0; i<this.requests.length; i++){
+    //   let ar = new ApprovalRequest();
+    //   // ar.date = this.requests[i].date;
+    //   ar.requesterName = this.requests[i].requesterName;
+    //   ar.requesterCompany = this.requests[i].company;
+    //   ar.nPersons = this.requests[i].persons.length;
+    //   ar.nVehicles = this.requests[i].vehicles.length;
+    //   ar.status = (i % 2 == 0);
+    //   ar.approvedDate = Date.now();
+    //   ar.approvedFrom = i.toString();
+    //   ar.reqID = this.requests[i].requestID;
+    //   this.approvalRequests.push(ar);
+    // }
 
     return this.approvalRequests;
   }
